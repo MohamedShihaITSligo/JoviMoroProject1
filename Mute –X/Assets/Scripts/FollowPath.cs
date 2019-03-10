696691886b16122c82e5dfb563cd692684f77200
MohamedShiha <46888852+MohamedShihaITSligo@users.noreply.github.com>
2019-03-10 09:20:40 +0000

    built the game structure
    added:
    - player with movements
    - Enemy with vision controller
     (can detect the player if not behind an object )

---
+++ Mute –X/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+    public NavigationPath path;
+    public bool PickRandomStartNode = false;
+    public bool canMove = true;
+    public float moveSpeed = 2f;
+    public float distanceToNodeTolerance = 0.2f;
+    public float stopDistance;
+
+    Vector2 currentTarget;
+    Rigidbody2D body;
+    int currentNodeIndex = 0;
+    //StopBox stopBox;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        if (PickRandomStartNode)
+        {
+            currentNodeIndex = Random.Range(1, path.NodeCount - 1);
+        }
+        GetNextNodePosition();
+        TeleportToNode();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position , currentTarget) <= distanceToNodeTolerance)
+        {
+            GetNextNodePosition();
+        }
+        //if (TraficLightGreen()) canMove = true;
+        //else canMove = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            body.MovePosition(Vector2.MoveTowards(transform.position, currentTarget, moveSpeed * Time.deltaTime));
+        }
+        body.angularVelocity = 0;
+    }
+
+    void TeleportToNode()
+    {
+        transform.position = currentTarget;
+    }
+
+    void GetNextNodePosition()
+    {
+        if (currentNodeIndex >= path.NodeCount) currentNodeIndex = 0;
+        currentTarget = path.GetNodePosition(currentNodeIndex);
+        transform.up = currentTarget - body.position;
+        currentNodeIndex++;
+    }
+
+    //bool TraficLightGreen()
+    //{
+    //    GameObject[] TraficLights = GameObject.FindGameObjectsWithTag("TrafficLight");
+    //    int shortestIndex = -1;
+    //    float shortestDistance = float.MaxValue;
+    //    for (int i = 0; i < TraficLights.Length; i++)
+    //    {
+    //        float trafficLightDistance = Vector2.Distance(transform.position, TraficLights[i].transform.position);
+    //        if (trafficLightDistance < shortestDistance)
+    //        {
+    //            shortestDistance = trafficLightDistance;
+    //            shortestIndex = i;
+    //        }
+    //    }
+    //    Color TraficLightColor = TraficLights[shortestIndex].GetComponent<TrafficLightController>().GetTrafgicLightState();
+    //    float distance = Vector2.Distance(transform.position, TraficLights[shortestIndex].transform.position);
+    //    if (distance < stopDistance && TraficLightColor.Equals(Color.red)) return false;
+    //    else return true;
+    //}
+}
---
+++ Mute –X/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float horizontal, vertical;
+    Rigidbody2D body;
+    
+
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	
+	void Update () {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, -(horizontal * 2));
+        body.velocity = transform.up * vertical * 5;
+        if (vertical == 0)  body.velocity = Vector2.zero;
+        body.angularVelocity = 0;
+    }
+
+}
+
