fe2ca837f49a20600dfeb759b32b8c8ec03adc0b
MohamedShiha <46888852+MohamedShihaITSligo@users.noreply.github.com>
2019-03-15 19:17:58 +0000

    changed the menu
    - added the menu background
    - added How to play menu backGround
    - the gameController is now in the maniMenu
    - volume slider now shows the value number
    - i need to work on the GameController to swich between levels

---
+++ Mute –X/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController: MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject howToPlay;
+    Slider volume;
+    Text txtVolume;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        howToPlay.SetActive(false);
+        volume = mainMenu.gameObject.GetComponentInChildren<Slider>();
+        txtVolume = volume.GetComponentInChildren<Text>();
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HowToPlay()
+    {
+        mainMenu.SetActive(false);
+        howToPlay.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        howToPlay.SetActive(false);
+    }
+
+    public void UpdateText()
+    {
+        float value = volume.value * 100;
+        txtVolume.text= value.ToString("0")+"%";
+    }
+}
